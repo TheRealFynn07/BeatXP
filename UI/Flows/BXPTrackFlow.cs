d10f0f6ce99011b13c8cefd2bb6969c68e0c0dcb
Riley Prince <NuggoDEV@gmail.com>
2024-05-28 04:18:45 +0930

    Add track flow & view

---
+++ UI/Flows/BXPTrackFlow.cs
@@ -0,0 +1,27 @@
+﻿using BeatSaberMarkupLanguage.Components;
+using BeatSaberMarkupLanguage.Tags;
+using BeatSaberMarkupLanguage.TypeHandlers;
+using BeatXP.UI.Views;
+using HMUI;
+using Zenject;
+
+namespace BeatXP.UI.Flows
+{
+    internal class BXPTrackFlow : FlowCoordinator
+    {
+        [Inject] private readonly BXPMainFlow _bxpMainFlow;
+        [Inject] private readonly BXPTrackView _bxpTrackView;
+
+        protected override void DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
+        {
+            if (!firstActivation)
+                return;
+
+            SetTitle("BeatXP - Track View");
+            showBackButton = true;
+
+            ProvideInitialViewControllers(_bxpTrackView);
+        }
+        protected override void BackButtonWasPressed(ViewController topViewController) => _bxpMainFlow.DismissFlowCoordinator(this);
+    }
+}
---
+++ UI/Views/BXPTrackView.cs
@@ -0,0 +1,11 @@
+﻿using BeatSaberMarkupLanguage.Attributes;
+using BeatSaberMarkupLanguage.ViewControllers;
+
+namespace BeatXP.UI.Views
+{
+    [HotReload(RelativePathToLayout = @"../TrackView.bsml")]
+    [ViewDefinition("BeatXP.UI.TrackView.bsml")]
+    internal class BXPTrackView : BSMLAutomaticViewController
+    {
+    }
+}
