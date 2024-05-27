935a5f754f8453f84b93094a64190b7afc4f0cc3
TheRealFynn07 <136192778+TheRealFynn07@users.noreply.github.com>
2024-05-27 18:18:46 +0200

    Initial commit

---
+++ BeatXPFlow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HMUI;
+using BeatSaberMarkupLanguage;
+
+namespace BeatXP
+{
+    class BeatXPFlow : HMUI.FlowCoordinator
+    {
+
+        protected override void DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling)
+        {
+            if (!firstActivation)
+                return;
+
+            SetTitle("BeatXP");
+            showBackButton = true;
+
+            ProvideInitialViewControllers(BeatSaberMarkupLanguage.BeatSaberUI.CreateViewController<BeatXPView>());
+
+        }
+
+        protected override void BackButtonWasPressed(ViewController topViewController)
+        {
+            BeatSaberMarkupLanguage.BeatSaberUI.MainFlowCoordinator.DismissFlowCoordinator(this);
+        }
+
+        internal void ShowFlow()
+        {
+            BeatSaberUI.PresentFlowCoordinator(BeatSaberUI.MainFlowCoordinator.YoungestChildFlowCoordinatorOrSelf(), this);
+        }
+    }
+}
---
+++ BeatXPView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using BeatSaberMarkupLanguage.ViewControllers;
+using BeatSaberMarkupLanguage.Attributes;
+
+namespace BeatXP
+{
+    [HotReload(RelativePathToLayout = @"./UI/UI.bsml")]
+    [ViewDefinition("BeatXP.UI.bsml")]
+    class BeatXPView : BSMLAutomaticViewController
+    {
+        void OpenDiscordLink() => Process.Start("https://discord.gg/tbJTZjjf6d");
+    }
+}
